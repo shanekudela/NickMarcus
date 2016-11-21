1ea7c0ca4e3b0e1fbbe75877c0889b563161818e
shanekudela <shane.kudela@rusd.org>
2016-11-21 11:46:41 -0600

    Started Whats a web page Today 11-21-2016
    See directions Whats a web page and use this file to completer

---
+++ WhatsAWebPage.py
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Supercool</title>
+    </head>
+    <body>
+        <h1> Surprise! Making a web page is easy!</h1>
+        You can put your content here.
+    </body>
+</html>
