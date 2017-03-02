44c014e697284e4083940453ee6a626774da86a3
NVenne <s163216@rusd.org>
2017-03-02 14:30:45 -0600

    Update 1.4.2 WOMAN CAT.py

--- 1.4.2 WOMAN CAT.py
+++ 1.4.2 WOMAN CAT.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-''' What is Kudelas Middle Name- guess here on line 3-
+''' What is Kudelas Middle Name- guess here on line 3-Steven
 JDoe_JSmith_1_4_2: Read and show an image.
 '''
 import matplotlib.pyplot as plt 
